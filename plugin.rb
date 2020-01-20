cbeebd042f75d6c80c5d905a54d5860504dde348
Xander <Xander@Atlantis.dev>
2020-01-19 21:38:35 -0500

    Adds plugin url

--- plugin.rb
+++ plugin.rb
@@ -4,5 +4,6 @@
 # about: Adds custom BBCode to Discourse for the LD4All community
 # version: 0.1
 # author: Xander
+# url: https://github.com/NXander/ld4all-bbcode
 
 register_asset "stylesheets/ld4all-bbcode.scss"
