66121854afcd0bf649e9ec8a1d0262ed19a28af3
Xan <11322064+NXander@users.noreply.github.com>
2026-09-20 21:34:47 -0700

    Bump plugin version from 1.3 to 1.4

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: ld4all-bbcode
 # about: Adds custom BBCode to Discourse for the LD4All community
-# version: 1.3
+# version: 1.4
 # authors: Xander
 # url: https://github.com/NXander/ld4all-bbcode
 
